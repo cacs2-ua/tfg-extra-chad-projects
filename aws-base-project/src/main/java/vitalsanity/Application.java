af7b096a2827e88626ea8cf044e192e830dadb78
Cristian Andrés Córdoba Silvestre <cacs2@alu.ua.es>
2025-02-01 11:00:51 +0100

    app akkas

--- aws-base-project/src/main/java/vitalsanity/Application.java
+++ aws-base-project/src/main/java/vitalsanity/Application.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("Aplicacion ejecutada correctamente y corriendo en: http://localhost:8002/aws-base");
+        System.out.println("Aplicacion ejecutada correctamente y corriendo en: http://localhost:8002/vital-sanity/login");
     }
 }
