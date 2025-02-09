4343205608503ac8a2fdaeaf03b4f22e1d0aba25
Cristian Andrés Córdoba Silvestre <cacs2@alu.ua.es>
2025-02-09 17:24:30 +0100

    volumen de docker para la persistencia GG

--- nice-despliegue-base/src/main/java/vitalsanity/initializer/DatabaseInitializer.java
+++ nice-despliegue-base/src/main/java/vitalsanity/initializer/DatabaseInitializer.java
@@ -12,7 +12,7 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 @Component
-@Profile("docker")  // Only run when the 'docker' profile is active
+@Profile("docker")  // Solo se ejecuta cuando el perfil 'docker' está activo
 public class DatabaseInitializer implements CommandLineRunner {
 
     @Autowired
@@ -20,19 +20,23 @@ public class DatabaseInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        // Load the SQL file from the classpath
+        // Comprobar si la tabla 'users' ya tiene datos
+        Integer count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users", Integer.class);
+        if (count != null && count > 0) {
+            System.out.println("La base de datos ya está inicializada. Se omite la ejecución del script de semilla.");
+            return;
+        }
+
+        // Cargar el archivo SQL desde el classpath
         ClassPathResource resource = new ClassPathResource("sql/database-script/seed-develop-db.sql");
 
-        // Read the file content into a string using InputStream
         try (InputStream inputStream = resource.getInputStream()) {
             String sql = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-
-            // Execute the SQL script
+            // Ejecuta el script SQL
             jdbcTemplate.execute(sql);
-            System.out.println("SQL script executed successfully!");
+            System.out.println("Script SQL ejecutado correctamente.");
         } catch (IOException e) {
-            System.err.println("Failed to read the SQL script: " + e.getMessage());
+            System.err.println("No se pudo leer el script SQL: " + e.getMessage());
         }
     }
 }
-
