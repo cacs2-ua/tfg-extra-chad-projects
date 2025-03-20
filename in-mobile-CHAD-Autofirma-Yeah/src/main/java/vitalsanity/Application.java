9ef9109368ce605c3ee1e60cf9baa8f5047527b6
Cristian Andrés Córdoba Silvestre <cacs2@alu.ua.es>
2025-03-20 17:51:17 +0100

    enables HTTPS in 'in-mobile-CHAD-Autofirma-Yeah'

--- in-mobile-CHAD-Autofirma-Yeah/src/main/java/vitalsanity/Application.java
+++ in-mobile-CHAD-Autofirma-Yeah/src/main/java/vitalsanity/Application.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("Aplicacion ejecutada correctamente y corriendo en: http://localhost:8616/vital-sanity/login");
+        System.out.println("Aplicacion ejecutada correctamente y corriendo en: https://localhost:8616/vital-sanity/login");
     }
 }
