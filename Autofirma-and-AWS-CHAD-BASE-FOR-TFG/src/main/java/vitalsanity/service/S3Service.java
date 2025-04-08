164c9fd01a626f8667389e78e29877ebfbda5176
Cristian Andrés Córdoba Silvestre <cacs2@alu.ua.es>
2025-04-08 18:38:48 +0200

    elimminado archivo que no se usaba

--- Autofirma-and-AWS-CHAD-BASE-FOR-TFG/src/main/java/vitalsanity/service/S3Service.java
+++ Autofirma-and-AWS-CHAD-BASE-FOR-TFG/src/main/java/vitalsanity/service/S3Service.java
@@ -52,20 +52,6 @@ public class S3Service {
         s3Client.putObject(putRequest, RequestBody.fromBytes(file.getBytes()));
     }
 
-        // Subir archivo a S3 con prefijo "informes/user-{userId}/..."
-        public void uploadTwoFile(Long userId, String fileName, MultipartFile file) throws IOException {
-                String key = "informes/user-" + userId + "/" + fileName;
-        
-                // Se indica el cifrado SSE-KMS, pero sin espeit stcificar la clave (se usará la configuración predeterminada del bucket)
-                PutObjectRequest putRequest = PutObjectRequest.builder()
-                        .bucket(bucketName)
-                        .key(key)
-                        .serverSideEncryption(ServerSideEncryption.AWS_KMS)
-                        .build();
-        
-                s3Client.putObject(putRequest, RequestBody.fromBytes(file.getBytes()));
-            }
-
 
     // Generar una URL pre-firmada para descargar el archivo
     public String generatePresignedUrl(Long userId, String s3Key, Duration duration) {
